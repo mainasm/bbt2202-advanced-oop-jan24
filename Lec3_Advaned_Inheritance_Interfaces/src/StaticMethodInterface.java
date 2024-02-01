81a70d769fe2bf3dbcb2bbb20406f7304b6b8ca4
Samuel Githogori <maina.sam@outlook.com>
2024-02-01 16:47:38 +0300

    added interface with static method

--- Lec3_Advaned_Inheritance_Interfaces/src/StaticMethodInterface.java
+++ Lec3_Advaned_Inheritance_Interfaces/src/StaticMethodInterface.java
@@ -1,5 +1,15 @@
-public class StaticMethodInterface {
-
-
-
+//An interface containing a static class
+interface MyEv {
+    static void batteryInfo() {
+        System.out.println("All electric vehicles use lithium batteries.");
+    }
 }
+
+class MyEvMain {
+    public static void main(String[] args) {
+        System.out.println("What type of batteries to electric vehicles use?");
+        MyEv.batteryInfo();
+    }
+}
+
+
